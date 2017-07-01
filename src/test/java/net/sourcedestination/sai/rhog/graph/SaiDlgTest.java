79e8eeb1d5a7bb6b667d25a4d1bfb68036679c01
jmorwick <jbmorwick@gmail.com>
2017-07-01 18:57:24 -0500

    prep for next tests

---
+++ src/test/java/net/sourcedestination/sai/rhog/graph/SaiDlgTest.java
@@ -0,0 +1,15 @@
+package net.sourcedestination.sai.rhog.graph;
+
+import org.junit.Test;
+
+/**
+ * Created by jmorwick on 7/1/17.
+ */
+public class SaiDlgTest {
+
+    @Test
+    public void testAdaptDLG() {
+        //TODO: implement
+    }
+
+}
