61009dbd3f2547d13fbbc2cf42ccbcde2136b75b
jmorwick <jbmorwick@gmail.com>
2018-06-18 23:53:28 -0500

    added AUDistance factory

--- src/main/java/net/sourcedestination/sai/rhog/comparison/distance/RhogDistanceAdapter.java
+++ src/main/java/net/sourcedestination/sai/rhog/comparison/distance/RhogDistanceAdapter.java
@@ -2,12 +2,24 @@ package net.sourcedestination.sai.rhog.comparison.distance;
 
 import dlg.bridges.GMLBridge;
 import dlg.core.DLG;
+import dlg.core.refinement.RefinementOperator;
+import dlg.core.subsumption.Subsumption;
+import dlg.ml.distance.AUDistance;
 import dlg.ml.distance.BagOfLabelsDistance;
 import dlg.ml.distance.NormalizedCompressionDistance;
+import dlg.util.Label;
+import net.sourcedestination.funcles.tuple.Tuple;
+import net.sourcedestination.funcles.tuple.Tuple2;
+import net.sourcedestination.sai.db.DBInterface;
 import net.sourcedestination.sai.db.graph.Graph;
 import net.sourcedestination.sai.experiment.retrieval.GraphSimilarityMetric;
+import net.sourcedestination.sai.rhog.ClassificationLabelLoader;
 import net.sourcedestination.sai.rhog.graph.DLGFactory;
 import net.sourcedestination.sai.rhog.graph.SaiDlg;
+import net.sourcedestination.sai.util.Task;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by jmorwick on 7/2/17.
@@ -38,5 +50,27 @@ public interface RhogDistanceAdapter extends GraphSimilarityMetric {
     static RhogDistanceAdapter getCompressionDistance() {
         NormalizedCompressionDistance d = new NormalizedCompressionDistance(new GMLBridge());
         return d::distance;
+    }
+
+    static Tuple2<RhogDistanceAdapter,Task> getAUDistance(Subsumption s,
+                                                          RefinementOperator r,
+                                                          DBInterface db,
+                                                          ClassificationLabelLoader c) {
+        AUDistance d = new AUDistance(s,r);
+        return Tuple.makeTuple(d::distance, new Task() {
+            @Override
+            public Object get() {
+                List<Graph> graphs = db.getGraphIDStream()
+                        .map(db::retrieveGraph)
+                        .collect(Collectors.toList());
+                List<Label> labels = graphs.stream()
+                        .map(c::apply)
+                        .map(l -> {
+                            try{ return new Label(l);}
+                            catch(Exception e) {return null;}
+                        }).collect(Collectors.toList());
+                return null;
+            }
+        });
     }
 }
