5532f14bacdd5e6c83c56d7879005a88dcb038a2
jmorwick <jbmorwick@gmail.com>
2018-06-19 19:54:00 -0500

    added URL based constructor and fixed bug in apply

--- src/main/java/net/sourcedestination/sai/rhog/ClassificationLabelLoader.java
+++ src/main/java/net/sourcedestination/sai/rhog/ClassificationLabelLoader.java
@@ -4,8 +4,10 @@ import net.sourcedestination.sai.db.graph.Feature;
 import net.sourcedestination.sai.db.graph.Graph;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -14,6 +16,10 @@ import java.util.function.Function;
 public class ClassificationLabelLoader implements Function<Graph,String> {
 
     private Map<String,String> labels = new HashMap<>();
+
+    public ClassificationLabelLoader(URL url) throws IOException {
+        this(url.openStream());
+    }
 
     public ClassificationLabelLoader(InputStream in) {
         BufferedReader rin = new BufferedReader(new InputStreamReader(in));
@@ -25,8 +31,13 @@ public class ClassificationLabelLoader implements Function<Graph,String> {
         });
     }
 
+    public String toString() {
+        return labels.toString();
+    }
+
     @Override
     public String apply(Graph g) {
-        return labels.get(g.getFeatures().filter(f -> f.getName().equals("label")).findFirst().get());
+        return labels.get(g.getFeatures()
+                .filter(f -> f.getName().equals("label")).findFirst().get().getValue());
     }
 }
