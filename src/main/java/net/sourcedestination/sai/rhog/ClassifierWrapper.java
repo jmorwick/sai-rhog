43f6a5ec83dd623c142d679285857967126c409c
jmorwick <jbmorwick@gmail.com>
2018-06-21 23:32:29 -0500

    added task for training classifiers

--- src/main/java/net/sourcedestination/sai/rhog/ClassifierWrapper.java
+++ src/main/java/net/sourcedestination/sai/rhog/ClassifierWrapper.java
@@ -2,9 +2,14 @@ package net.sourcedestination.sai.rhog;
 
 import dlg.core.DLG;
 import dlg.ml.Classifier;
+import net.sourcedestination.sai.db.DBInterface;
 import net.sourcedestination.sai.db.graph.Graph;
 import net.sourcedestination.sai.experiment.learning.ClassificationModel;
 import net.sourcedestination.sai.rhog.graph.DLGFactory;
+import net.sourcedestination.sai.rhog.graph.SaiDlg;
+import net.sourcedestination.sai.util.Task;
+
+import java.util.stream.Collectors;
 
 public class ClassifierWrapper  implements ClassificationModel {
 
@@ -24,6 +29,25 @@ public class ClassifierWrapper  implements ClassificationModel {
                 return internalClassifier.predict((DLG) g).get();
             return internalClassifier.predict(factory.apply(g)).get();
         } catch(Exception e) { throw new RuntimeException(e); }
+    }
+
+    public Task train(DBInterface db, ClassificationLabelLoader classes) {
+        return () -> {
+            try {
+                internalClassifier.train(
+                        db.getGraphIDStream()
+                                .map(db::retrieveGraph)
+                                .map(g -> g instanceof DLG ? (DLG) g : (DLG) factory.apply(g))
+                                .collect(Collectors.toList()),
+
+                        db.getGraphIDStream()
+                                .map(db::retrieveGraph)
+                                .map(classes::apply)
+                                .map(SaiDlg::labelFromString)
+                                .collect(Collectors.toList()));
+            } catch(Exception e) { throw new RuntimeException(e); }
+            return null;
+        };
     }
 
 }
