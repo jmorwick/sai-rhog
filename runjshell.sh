608b914a7040493ba694c1aca4ab335b3d6818b8
jmorwick <jbmorwick@gmail.com>
2018-07-03 15:08:26 -0500

    no longer needed

--- runjshell.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-jshell --class-path=~/.m2/repository/net/sourcedestination/sai-rhog/1.0-SNAPSHOT/sai-rhog-1.0-SNAPSHOT.jar:~/.m2/repository/net/sourcedestination/sai/2.0-SNAPSHOT/sai-2.0-SNAPSHOT.jar:~/.m2/repository/net/sourcedestination/funcles/2.0/funcles-2.0.jar:lib/rhog-2017-07-01.jar:~/.m2/repository/com/google/guava/guava/22.0/guava-22.0.jar $1
